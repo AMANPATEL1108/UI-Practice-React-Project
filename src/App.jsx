921f24a299a2df0e89665ff494b3ba0e0f7faf39
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-06-16 16:09:29 +0530

    Corerctly Worked

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hii Aman</h1>
+
+      <h1 class="text-3xl font-bold ml-10">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
