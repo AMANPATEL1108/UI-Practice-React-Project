0e3d23143ad202928eb1928eb9fa9dd5ae882a70
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-06-16 16:13:18 +0530

    Add talwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Hii Aman</h1>
+      <h1 className="bg-amber-300">Hii Aman</h1>
 
-      <h1 class="text-3xl font-bold ml-10">Hello world!</h1>
+      <h1 class="text-3xl font-bold m-10">Hello world!</h1>
     </>
   );
 }
